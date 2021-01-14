6b82864a2f2d01aeb5d12612816d53c3da1a6551
walden tang <walden@buaa.edu.cn>
2021-01-14 11:03:35 +0800

    Update smpl_gen.cc
    remove info

--- smpl_gen.cc
+++ smpl_gen.cc
@@ -1,21 +1,3 @@
-/*----------------------------------------------------------------------------------------------
-*
-* This file is ArcSoft's property. It contains ArcSoft's trade secret, proprietary and
-* confidential information.
-*
-* The information and code contained in this file is only for authorized ArcSoft employees
-* to design, create, modify, or review.
-*
-* DO NOT DISTRIBUTE, DO NOT DUPLICATE OR TRANSMIT IN ANY FORM WITHOUT PROPER AUTHORIZATION.
-*
-* If you are not an intended recipient of this file, you must not copy, distribute, modify,
-* or take any action in reliance on it.
-*
-* If you have received this file in error, please immediately notify ArcSoft and
-* permanently delete the original and any copy of any file and any printout thereof.
-*
-*-------------------------------------------------------------------------------------------------*/
-
 
 
 #include "smpl_model.h"
@@ -149,7 +131,7 @@ void process_single(int argc, char *argv[])
 	smpl.setShape(beta);
 	smpl.setPose(pose);
 
-	//Ëæ»ú×ËÌ¬
+	//Ã‹Ã¦Â»ÃºÃ—Ã‹ÃŒÂ¬
 	//smpl.randomShape();
 	//smpl.randomPose();
 
